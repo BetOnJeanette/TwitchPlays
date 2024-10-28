8764b58de541e3ba9add258f0601a2cc190ae719
= <=>
2024-10-28 17:18:03 -0400

    Separate the game-specific handling from the core handling functionality

--- TwitchPlays_TEMPLATE.py
+++ TwitchPlays_TEMPLATE.py
@@ -1,9 +1,8 @@
 import concurrent.futures
 import keyboard
-import pydirectinput
 import pyautogui
-import TwitchPlays_Connection
-from TwitchPlays_KeyCodes import *
+import time
+from Handlers.Gtav import GTAVMessageHandler
 from StreamConnections import StreamConnectionFactory
 
 ##################### MESSAGE QUEUE VARIABLES #####################
@@ -38,69 +37,11 @@ while countdown > 0:
 
 t = StreamConnectionFactory.getJeansTwitch()
 
+
 def handle_message(message):
     try:
-        msg = message['message'].lower()
-        username = message['username'].lower()
-
-        print("Got this message from " + username + ": " + msg)
-
-        # Now that you have a chat message, this is where you add your game logic.
-        # Use the "HoldKey(KEYCODE)" function to permanently press and hold down a key.
-        # Use the "ReleaseKey(KEYCODE)" function to release a specific keyboard key.
-        # Use the "HoldAndReleaseKey(KEYCODE, SECONDS)" function press down a key for X seconds, then release it.
-        # Use the pydirectinput library to press or move the mouse
-
-        # I've added some example videogame logic code below:
-
-        ###################################
-        # Example GTA V Code 
-        ###################################
-
-        # If the chat message is "left", then hold down the A key for 2 seconds
-        if msg == "left": 
-            HoldAndReleaseKey(A, 2)
-
-        # If the chat message is "right", then hold down the D key for 2 seconds
-        if msg == "right": 
-            HoldAndReleaseKey(D, 2)
-
-        # If message is "drive", then permanently hold down the W key
-        if msg == "drive": 
-            ReleaseKey(S) #release brake key first
-            HoldKey(W) #start permanently driving
-
-        # If message is "reverse", then permanently hold down the S key
-        if msg == "reverse": 
-            ReleaseKey(W) #release drive key first
-            HoldKey(S) #start permanently reversing
-
-        # Release both the "drive" and "reverse" keys
-        if msg == "stop": 
-            ReleaseKey(W)
-            ReleaseKey(S)
-
-        # Press the spacebar for 0.7 seconds
-        if msg == "brake": 
-            HoldAndReleaseKey(SPACE, 0.7)
-
-        # Press the left mouse button down for 1 second, then release it
-        if msg == "shoot": 
-            pydirectinput.mouseDown(button="left")
-            time.sleep(1)
-            pydirectinput.mouseUp(button="left")
-
-        # Move the mouse up by 30 pixels
-        if msg == "aim up":
-            pydirectinput.moveRel(0, -30, relative=True)
-
-        # Move the mouse right by 200 pixels
-        if msg == "aim right":
-            pydirectinput.moveRel(200, 0, relative=True)
-
-        ####################################
-        ####################################
-
+        print(message)
+        GTAVMessageHandler.handleMessage(message)
     except Exception as e:
         print("Encountered exception: " + str(e))
 
@@ -109,10 +50,10 @@ while True:
 
     active_tasks = [t for t in active_tasks if not t.done()]
 
-    #Check for new messages
-    new_messages = t.twitch_receive_messages();
+    # Check for new messages
+    new_messages = t.twitch_receive_messages()
     if new_messages:
-        message_queue += new_messages; # New messages are added to the back of the queue
+        message_queue += new_messages # New messages are added to the back of the queue
         message_queue = message_queue[-MAX_QUEUE_LENGTH:] # Shorten the queue to only the most recent X messages
 
     messages_to_handle = []
@@ -127,7 +68,7 @@ while True:
             # Pop the messages we want off the front of the queue
             messages_to_handle = message_queue[0:n]
             del message_queue[0:n]
-            last_time = time.time();
+            last_time = time.time()
 
     # If user presses Shift+Backspace, automatically end the program
     if keyboard.is_pressed('shift+backspace'):
@@ -141,4 +82,3 @@ while True:
                 active_tasks.append(thread_pool.submit(handle_message, message))
             else:
                 print(f'WARNING: active tasks ({len(active_tasks)}) exceeds number of workers ({MAX_WORKERS}). ({len(message_queue)} messages in the queue)')
- 
